e2bebe258e9f0c84da0f9dafda5904079ebca83d
Lucy20209060 <550502661@qq.com>
2018-01-03 21:57:56 +0800

    event

---
+++ nodeTest/events.js
@@ -0,0 +1,11 @@
+const EventEmitter = require('events');
+
+class Player extends EventEmitter {}
+
+var player = new Player();
+
+player.on('play',() => {
+    console.log('正在播放')
+})
+
+player.emit('play')
