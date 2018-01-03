3d38a600959df1e70222abfa096dd988b827d8c4
Lucy20209060 <550502661@qq.com>
2018-01-03 22:02:51 +0800

    emit

--- nodeTest/events.js
+++ nodeTest/events.js
@@ -4,8 +4,10 @@ class Player extends EventEmitter {}
 
 var player = new Player();
 
-player.on('play',() => {
-    console.log('正在播放')
+player.on('play',(par) => {
+    console.log(`正在播放-${par}...`)
 })
 
-player.emit('play')
+// 对此触发 emit 多次调用play函数
+player.emit('play','父亲写的的散文诗')
+player.emit('play','天空之城')
