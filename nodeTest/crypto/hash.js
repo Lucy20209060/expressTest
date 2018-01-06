b167f30a9819ec03dc3fa0840a54cf1f20a3d149
luc <550502661@qq.com>
2018-01-06 23:59:21 +0800

    加密算法

---
+++ nodeTest/crypto/hash.js
@@ -0,0 +1,82 @@
+const crypto = require('crypto');
+const hash = crypto.createHash('md5');
+
+// MD5和SHA1
+// MD5是一种常用的哈希算法，用于给任意数据一个“签名”。这个签名通常用一个十六进制的字符串表示：
+// 可任意多次调用update():
+// hash.update('Hello, world!');
+// hash.update('Hello, nodejs!');
+
+// console.log(hash.digest('hex'));
+
+/*
+update()方法默认字符串编码为UTF-8，也可以传入Buffer。
+如果要计算SHA1，只需要把'md5'改成'sha1'，就可以得到SHA1的结果1f32b9c9932c02227819a4151feed43e131aca40。
+还可以使用更安全的sha256和sha512。
+*/
+
+// Hmac
+// Hmac算法也是一种哈希算法，它可以利用MD5或SHA1等哈希算法。不同的是，Hmac还需要一个密钥：
+const hmac = crypto.createHmac('sha256', 'secret-key');
+
+hmac.update('Hello, world!');
+hmac.update('Hello, nodejs!');
+
+console.log(hmac.digest('hex'));
+// 只要密钥发生了变化，那么同样的输入数据也会得到不同的签名，因此，可以把Hmac理解为用随机数“增强”的哈希算法。
+
+
+// AES
+// AES是一种常用的对称加密算法，加解密都用同一个密钥。crypto模块提供了AES支持，但是需要自己封装好函数，便于使用：
+function aesEncrypt(data, key) {
+    const cipher = crypto.createCipher('aes192', key);
+    var crypted = cipher.update(data, 'utf8', 'hex');
+    crypted += cipher.final('hex');
+    return crypted;
+}
+
+function aesDecrypt(encrypted, key) {
+    const decipher = crypto.createDecipher('aes192', key);
+    var decrypted = decipher.update(encrypted, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+}
+
+// 需要加密的数据 和 密钥
+var data = 'Hello, this is a secret message!';
+var key = 'Password';
+
+var encrypted = aesEncrypt(data, key);
+var decrypted = aesDecrypt(encrypted, key);
+
+console.log('Plain text: ' + data);
+console.log('Encrypted text: ' + encrypted);
+// 加密后的字符串通过解密得到原始内容
+console.log('Decrypted text: ' + decrypted);
+
+// AES有很多不同的算法，如aes192，aes-128-ecb，aes-256-cbc等
+
+
+// Diffie-Hellman
+// DH算法是一种密钥交换协议，它可以让双方在不泄漏密钥的情况下协商出一个密钥来。DH算法基于数学原理
+// xiaoming's keys:
+var ming = crypto.createDiffieHellman(512);
+var ming_keys = ming.generateKeys();
+
+var prime = ming.getPrime();
+var generator = ming.getGenerator();
+
+console.log('Prime: ' + prime.toString('hex'));
+console.log('Generator: ' + generator.toString('hex'));
+
+// xiaohong's keys:
+var hong = crypto.createDiffieHellman(prime, generator);
+var hong_keys = hong.generateKeys();
+
+// exchange and generate secret:
+var ming_secret = ming.computeSecret(hong_keys);
+var hong_secret = hong.computeSecret(ming_keys);
+
+// print secret:
+console.log('Secret of Xiao Ming: ' + ming_secret.toString('hex'));
+console.log('Secret of Xiao Hong: ' + hong_secret.toString('hex'));
