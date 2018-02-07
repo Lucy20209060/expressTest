fc43690236b3f858b536c6c43cb7358a6971e2d3
Lucy20209060 <550502661@qq.com>
2018-02-08 00:44:58 +0800

    登陆页

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ var app = express();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+// var login = require('./routes/login');
 app.use('/', index);
 app.use('/users', users);
+// app.use('/login', login);
 
 
 // view engine setup
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'indexPage' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: '登陆页' });
+});
+
 module.exports = router;
