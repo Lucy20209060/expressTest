3859a9917910592cb2696ac4b7c7a1263b813285
Lucy20209060 <550502661@qq.com>
2018-04-13 18:08:16 +0800

    接口模拟

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+// success
+var SUCCESS = {
+    status:0,
+    success:true
+}
+// error
+var ERROR = {
+    status:1,
+    success:false
+}
+
 /* GET users page. */
 router.get('/', function(req, res, next) {
     res.render('users', { title: 'usersPage',friends:1 });
@@ -13,5 +24,37 @@ router.get('/list', function(req, res, next) {
 // /users/ab*****ut 模糊匹配
 router.get('/ab*ut',function(req,res,next){
     res.send('模糊匹配')
+})
+
+// 商品信息
+router.get('/productDet', function (req, res, next) {
+    let productId = req.query.productId
+
+    if(productId === undefined){
+        res.json({
+            status: 1,
+            msg: 'suc',
+            result: productId,
+            success:false,
+            msg:'没有商品ID'
+        })
+    }else{
+        res.json(
+            Object.assign(
+                {
+                    msg: '成功',
+                    id: productId,
+                    name:'临安小番薯500g*3甜在心头',
+                    data:{
+                        a:1,
+                        b:2,
+                        c:3
+                    }
+                },
+                SUCCESS
+            )
+        )
+    }
+    
 })
 module.exports = router;
