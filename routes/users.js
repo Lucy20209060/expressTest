29f2ee5880382d2c49df40d673296660140c1270
Lucy20209060 <550502661@qq.com>
2019-02-28 16:40:54 +0800

    feat:express连接数据库

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var dbConfig = require('../db/dbConfig');
+
+// 使用DBConfig.js的配置信息创建一个MySQL连接池
+var pool = mysql.createPool( dbConfig.mysql );
 
 // success
 var SUCCESS = {
@@ -28,33 +33,46 @@ router.get('/ab*ut',function(req,res,next){
 
 // 商品信息
 router.get('/productDet', function (req, res, next) {
-    let productId = req.query.productId
-		if(productId === undefined || productId === '' || Number(productId) === NaN){
-        res.json(
-            Object.assign(
-                {
-                    msg:'商品ID有误'
-                },
-                ERROR
-            )
-        )
-    }else{
-        res.json(
-            Object.assign(
-                {
-                    msg: '成功',
-                    id: productId,
-                    name:'临安小番薯500g*3甜在心头',
-                    data:{
-                        a:1,
-                        b:2,
-                        c:3
-                    }
-                },
-                SUCCESS
-            )
-        )
-    }
+
+    pool.getConnection(function(err, connection) {
+        connection.query('select id,code,name from menu;', '', function(err, result) {
+            // responseJSON(res, result);   
+            // console.log(json(result))
+            res.json(result)
+            // 释放连接  
+            connection.release();  
+        })
+    })
+
+
+
+    // let productId = req.query.productId
+	// 	if(productId === undefined || productId === '' || Number(productId) === NaN){
+    //     res.json(
+    //         Object.assign(
+    //             {
+    //                 msg:'商品ID有误'
+    //             },
+    //             ERROR
+    //         )
+    //     )
+    // }else{
+    //     res.json(
+    //         Object.assign(
+    //             {
+    //                 msg: '成功',
+    //                 id: productId,
+    //                 name:'临安小番薯500g*3甜在心头',
+    //                 data:{
+    //                     a:1,
+    //                     b:2,
+    //                     c:3
+    //                 }
+    //             },
+    //             SUCCESS
+    //         )
+    //     )
+    // }
     
 })
 module.exports = router;
